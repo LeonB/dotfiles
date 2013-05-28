4fe14b0fc8b4906d6964ffad251ad2ce95256a3e
Leon Bogaert <leon@tim-online.nl>
2013-05-28 23:01:43 +0200

    Work with new bin location

--- .bashrc.d/paths.sh
+++ .bashrc.d/paths.sh
@@ -1,6 +1,4 @@
-PATH=$HOME/Software/local/bin:$PATH
-PATH=$HOME/Software/bin:$PATH
-#PATH=$HOME/Software/googleappengine.svn/python:$PATH
+PATH=$HOME/bin:$PATH
 PATH=$HOME/Software/google_appengine:$PATH
 PATH=$HOME/.local/bin:$PATH
 #PATH=/opt/vagrant/bin:$PATH
