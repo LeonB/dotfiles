7bf0dd7a3f3ed95e7ccb51fa1af621a64048dd33
Leon Bogaert <leon@tim-online.nl>
2013-10-20 21:50:31 +0200

    Clean it up a little bit

--- bin/chrome-clean-history.sh
+++ bin/chrome-clean-history.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+sqlite_command="delete from urls where last_visit_time <= (strftime('%s',(select max(last_visit_time)/10000000 from urls),'unixepoch','-31 days')*10000000);"
+
 #Kill all chrome instances
 /usr/bin/killall {chromium-browser,chrome} --quiet
 
 #Clean up sqlite databases
-/usr/bin/find $HOME/.config/{chromium,google-chrome}/Default -type f -name "History" -o -name "History Index *" -o -name "Archived History" -exec sqlite3 {} "delete from urls where last_visit_time <= (strftime('%s',(select max(last_visit_time)/10000000 from urls),'unixepoch','-31 days')*10000000);" \;
+/usr/bin/find $HOME/.config/{chromium,google-chrome}/Default -type f -name "History" -o -name "History Index *" -o -name "Archived History" -exec sqlite3 {} "$sqlite_command" \;
 
 #Remove history database
-#rm ~/.config/chromium/Default/History\ Index\ *
 /usr/bin/find $HOME/.config/{chromium,google-chrome}/Default -type f -name "History Index *" -exec rm {} \;
