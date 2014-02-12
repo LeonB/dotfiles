5b685a8bb284f3a268ec1b20b9ddc35c7036906c
Leon Bogaert <leon@tim-online.nl>
2014-02-12 22:07:17 +0100

    Made importing of certs more robust

--- bin/importcert.sh
+++ bin/importcert.sh
@@ -1,23 +1,65 @@
 #!/bin/sh
 #
-# usage:  import-cert.sh remote.host.name [port]
+# This script adds an ssl certificate to the linux NSS Shared DB of certs.
 #
+# usage:  import-cert.sh remote.host.name [port]
+# you may need to intall libnss3-tools and curl
+#
+# To see which certs are in your DB:   certutil -L -d sql:$HOME/.pki/nssdb
+# To delete a cert from your DB:       certutil -D -n <THE_NAME> -d sql:$HOME/.pki/nssdb
+# To list details of a cert:           certutil -d sql:$HOME/.pki/nssdb -L -n  <THE_NAME>
+
+# This script was taken from: http://blog.avirtualhome.com/2010/02/02/adding-ssl-certificates-to-google-chrome-linux-ubuntu/
+# This page may also be of help: http://code.google.com/p/chromium/wiki/LinuxCertManagement
+
+NSSDB=$HOME/.pki/nssdb
+
+#create cert directory if not found
+if [ ! -e $NSSDB ]
+then
+echo -e “===========================\nNo Database found. Creating one…\n=================================”
+mkdir -p $NSSDB
+cd $NSSDB
+certutil -N -d sql:.
+fi
+
 REMHOST=$1
+
+#is not host is given list the certs we have
+if [ -z $REMHOST ]; then
+    certutil -L -d sql:$NSSDB
+    exit 0
+fi
+
+#download cert
 REMPORT=${2:-443}
+echo "Downloading cert from ${REMHOST}:${REMPORT}"
 exec 6>&1
 exec > $REMHOST
 echo | openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
-certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "$REMHOST" -i $REMHOST 
-#certutil -d sql:$HOME/.pki/nssdb -A -t "TC,," -n "$REMHOST" -i $REMHOST 
+
+# Check if certificate already exist
+# If it does: delete it so that importing it again
+# doesn't throw an error
+certutil -d sql:$NSSDB -L -n "$REMHOST" 2>&1 /dev/null
+if [ $? -eq 0 ]; then
+    echo "Certificate ${REMHOST} already exists: going to delete it"
+    certutil -d sql:$NSSDB -D -n "$REMHOST"
+fi
+
+# add cert
+echo "Importing cert"
+
+# Add trusted CA to client & servert certs (implies c)
+certutil -d sql:$NSSDB -A -t TC -n "$REMHOST" -i $REMHOST
+
+# Import root certificate
+# certutil -d sql:$NSSDB -A -t "C,," -n "$REMHOST" -i $REMHOST
+
+# Import self-signed certificate
+# certutil -d sql:$NSSDB -A -t "P,," -n "$REMHOST" -i $REMHOST
+
 exec 1>&6 6>&-
 
-#Root certificate
-#https://windows.box.com/certsrv
-#https://windows.box.com/certsrv/certnew.cer?ReqID=CACert&Renewal=0&Mode=inst&Enc=b64
-#certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -i /home/leon/Downloads/certnew\ \(1\).cer -n Tim_online
-
-#wget -O cacert-root.crt "http://www.cacert.org/certs/root.crt"
-#wget -O cacert-class3.crt "http://www.cacert.org/certs/class3.crt"
-
-#certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "CAcert.org" -i cacert-root.crt 
-#certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "CAcert.org Class 3" -i cacert-class3.crt
+# Clean up after yourself
+find . -maxdepth 1 -type f -name "$REMHOST" -mmin -1 -delete
