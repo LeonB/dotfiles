414fe6c790d77811b6b90b95b1e39e661bbbe14c
Leon Bogaert <leon@tim-online.nl>
2014-05-31 23:55:51 +0200

    Added command to automatically update heroku plugins

---
+++ bin/heroku_update_plugins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASEPATH=$HOME/.heroku/plugins
+cd $BASEPATH
+
+for i in $(find ./* -maxdepth 0 -type d); do
+    name=${i:2}
+	cd "$BASEPATH/$name/"
+	echo "Updating $name"
+
+    git fetch
+    git reset --hard FETCH_HEAD
+    git clean -df
+done
