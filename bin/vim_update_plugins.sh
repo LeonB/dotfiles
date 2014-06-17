dfea8a200f853260f02cc5e8325c9cb2a819ddb2
Leon Bogaert <leon@tim-online.nl>
2014-06-17 09:39:10 +0200

    Use neobundle for updating
    Don't use raw git. NeoBundle does some additional things (compiling stuff for
    example)

--- bin/vim_update_plugins.sh
+++ bin/vim_update_plugins.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-BASEPATH=$HOME/.vim/bundle/
-cd $BASEPATH
-
-for i in $(find ./* -maxdepth 0 -type d); do
-    name=${i:2}
-	cd "$BASEPATH/$name/"
-	echo "Updating $name"
-
-    git fetch
-    git reset --hard FETCH_HEAD
-    git clean -df
-done
+vim -c "NeoBundleUpdate | q"
