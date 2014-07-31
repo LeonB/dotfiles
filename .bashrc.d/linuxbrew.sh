4c3dbd607743c31204270ebbdfacc609a00cbe49
Leon Bogaert <leon@tim-online.nl>
2014-07-31 23:59:26 +0200

    Disable linuxbrew for now

--- .bashrc.d/linuxbrew.sh
+++ .bashrc.d/linuxbrew.sh
@@ -1,3 +1,4 @@
+return # disable linuxbrew for now
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export LD_LIBRARY_PATH="$HOME/.linuxbrew/lib:$LD_LIBRARY_PATH"
 source `brew --repository`/Library/Contributions/brew_bash_completion.sh
