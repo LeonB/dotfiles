b6d54a5085fc65b94610147ced6d50c80e786668
Leon Bogaert <leon@tim-online.nl>
2014-09-01 17:04:41 +0200

    Pull in all work related repositories

---
+++ bin/pull_all_timonline_repos.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# apt-get install python-bitbucket
+
+repos=`python <<EOF
+import bitbucket
+import os
+
+bb = bitbucket.BitBucket(os.environ.get('BITBUCKET_USERNAME'),
+    os.environ.get('BITBUCKET_PASSWORD'))
+for repo in bb.user('Tim_online').repositories():
+    print repo['slug']
+EOF`
+
+# Sort based on repo name
+repos=`echo "${repos}" | sort`
+
+for repo in $repos
+do
+    echo -n "Checking $repo"
+
+    if [ ! -d "${repo}" ]; then
+        echo " (cloning)"
+        git clone "git@bitbucket.org:tim_online/${repo}.git" > /dev/null
+    else
+        echo " (updating)"
+        cd "${repo}"
+        git fetch --all > /dev/null
+        git reset --hard FETCH_HEAD > /dev/null
+        git clean -df > /dev/null
+        cd ..
+    fi
+done
