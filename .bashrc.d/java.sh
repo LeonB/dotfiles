2e15c8537bb2b10c28f2b7b689bad5d6fa00ea2d
Leon Bogaert <leon@tim-online.nl>
2014-09-27 00:53:13 +0200

    Trying some stuff for phpstorm

---
+++ .bashrc.d/java.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=$HOME/bin/tuxjdk
+export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on \
+                      -Dswing.aatext=true \
+                      -Dsun.java2d.xrender=true \
+                      -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
