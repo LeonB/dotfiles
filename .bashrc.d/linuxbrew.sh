cf8452fef1daa79c2b8908ddf1ac29edf6355a9a
Leon Bogaert <leon@tim-online.nl>
2014-10-09 14:05:44 +0200

    Try linuxbrew again

--- .bashrc.d/linuxbrew.sh
+++ .bashrc.d/linuxbrew.sh
@@ -1,4 +1,4 @@
-return # disable linuxbrew for now
+# return # disable linuxbrew for now
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export LD_LIBRARY_PATH=`smart_append LD_LIBRARY_PATH $HOME/.linuxbrew/.lib`
 source `brew --repository`/Library/Contributions/brew_bash_completion.sh
