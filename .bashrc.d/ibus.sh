11da77478f38777dcbaa1b769639b8f2dc2e107b
Leon Bogaert <leon@tim-online.nl>
2014-11-25 09:50:39 +0100

    Trying to prevent chromium lockups / crashes

---
+++ .bashrc.d/ibus.sh
@@ -0,0 +1,5 @@
+# Fix for chromium bug:
+# https://bugs.launchpad.net/ubuntu/+source/chromium-browser/+bug/1307648
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export CLUTTER_IM_MODULE=ibus
