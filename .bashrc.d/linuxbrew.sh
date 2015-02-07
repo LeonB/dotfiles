f011fcfe05befdfe32877d0519c94e957be263e4
Leon Bogaert <leon@tim-online.nl>
2015-02-08 00:57:15 +0100

    Fixed typo

--- .bashrc.d/linuxbrew.sh
+++ .bashrc.d/linuxbrew.sh
@@ -1,5 +1,5 @@
 # return # disable linuxbrew for now
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export HOMEBREW_LOGS="$HOME/.linuxbrew/Logs"
-export LD_LIBRARY_PATH=`smart_append LD_LIBRARY_PATH $HOME/.linuxbrew/.lib`
+export LD_LIBRARY_PATH=`smart_append LD_LIBRARY_PATH $HOME/.linuxbrew/lib`
 source `brew --repository`/Library/Contributions/brew_bash_completion.sh
