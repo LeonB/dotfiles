761da4f3dd04d75222546858e38cb41338b1edd7
Leon Bogaert <leon@tim-online.nl>
2015-03-08 20:16:58 +0100

    Disable GOROOT

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,3 +1,19 @@
 export GOPATH=$HOME/.go
 export PATH="$GOPATH/bin:$PATH"
-export GOROOT="/usr/local/go"
+# export GOROOT="/usr/local/go"
+
+# program_exists () {
+#     type "$1" &> /dev/null ;
+# }
+
+# if program_exists go; then
+#     function setupGOROOT()
+#     {
+#         local GOPATH=`which go`
+#         local GODIR=`dirname $GOPATH`
+#         local GOPATH_BREW_RELATIVE=`readlink $GOPATH`
+#         local GOPATH_BREW=`dirname $GOPATH_BREW_RELATIVE`
+#         export GOROOT=`cd $GODIR; cd $GOPATH_BREW/..; pwd`
+#     }
+#     setupGOROOT
+# fi
