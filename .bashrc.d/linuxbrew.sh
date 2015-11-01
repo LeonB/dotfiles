ab32cfd418f94c3d4f0869d44f4429bd7bf712ed
Leon Bogaert <leon@tim-online.nl>
2015-11-01 01:00:20 +0100

    test

--- .bashrc.d/linuxbrew.sh
+++ .bashrc.d/linuxbrew.sh
@@ -1,5 +1,19 @@
 # return # disable linuxbrew for now
-export PATH="$HOME/.dotfiles/.linuxbrew/bin:$PATH"
-export HOMEBREW_LOGS="$HOME/.linuxbrew/Logs"
+# export PATH="$HOME/.dotfiles/.linuxbrew/bin:$PATH"
+# # export LD_LIBRARY_PATH=`smart_append LD_LIBRARY_PATH /usr/lib`
 # export LD_LIBRARY_PATH=`smart_append LD_LIBRARY_PATH $HOME/.linuxbrew/lib`
-source `brew --repository`/Library/Contributions/brew_bash_completion.sh
+# export C_INCLUDE_PATH="$HOME/.linuxbrew/include"
+
+if [ -d $HOME/.linuxbrew/ ];then
+    LINUXBREW="$HOME/.dotfiles/.linuxbrew"
+    export HOMEBREW_LOGS="$LINUXBREW/Logs"
+
+    export PATH="$LINUXBREW/bin:$PATH"
+    export C_INCLUDE_PATH="$LINUXBREW/include:$C_INCLUDE_PATH"
+    export LIBRARY_PATH="$LINUXBREW/lib/:$LIBRARY_PATH"
+
+    export MANPATH="$LINUXBREW/share/man:$MANPATH"
+    export INFOPATH="$LINUXBREW/share/info:$INFOPATH"
+
+    source `brew --repository`/Library/Contributions/brew_bash_completion.sh
+fi
