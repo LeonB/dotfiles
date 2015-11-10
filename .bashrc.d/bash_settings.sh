0a3531092b783eaf4fa2e0d1847dcdcc56e23cc7
Leon Bogaert <leon@tim-online.nl>
2015-11-10 21:30:57 +0100

    Some experimentation with bash history

--- .bashrc.d/bash_settings.sh
+++ .bashrc.d/bash_settings.sh
@@ -1,6 +1,11 @@
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=100000
 HISTFILESIZE=20000
+
+# Create history file per year/month
+# HISTFILE="${HOME}/.history/$(date -u +%Y/%m/%d.%H.%M.%S)_$(hostname)_$$"
+# mkdir -p $(dirname "$HISTFILE")
+
 TERM=xterm-256color
 
 # let * also match filenames beginning with a dot
