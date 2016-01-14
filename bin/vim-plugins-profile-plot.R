2abde72698e1d0914f69b74444f2e0917dab03c7
Leon Bogaert <leon@tim-online.nl>
2016-01-14 16:57:47 +0100

    Visualisation of vim startup time

---
+++ bin/vim-plugins-profile-plot.R
@@ -0,0 +1,86 @@
+if("ggplot2" %in% rownames(installed.packages()) == FALSE) {
+
+  isOk <- FALSE
+  # we need to install the ggplot2 package
+  while(!isOk){
+    message("Trying in install the required R:ggplot2 package. Confirm? (y/n)")
+    ANSWER <- readLines(con="stdin", 1)
+    if (tolower(substr(ANSWER, 1,1)) == "y" ) {
+      isOk = TRUE
+      message("Confirmed to install.")
+      message(" ")
+      message("==========================================\n")
+      message("Installing required R dependency package...\n")
+
+      # install the required package if not present from the default package repository
+      install.packages("ggplot2", repos="http://cran.rstudio.com/", dep = TRUE)
+      message("Installation  finished!\n")
+      message("==========================================\n")
+    }
+    else {
+      isOk = FALSE
+      message("You did not say yes, so aborting.")
+      return(-1)
+    }
+  }
+}
+
+
+out <- tryCatch(
+                {
+                  require(ggplot2)
+                  dat <- read.csv("profile.csv", header = FALSE, col.names = c("TraceTime", "SourceTime", "ExecTime", "PluginName"))
+                  dat.n <- aggregate(ExecTime ~ PluginName, data = dat, "sum")
+                  
+                  # now plot!
+                  p <- ggplot(dat.n, aes(x = PluginName, y = ExecTime, fill = ExecTime)) + 
+                    geom_bar(stat="identity") + 
+                    coord_flip()
+                  
+                  # add in the colors!
+                  p <- p + scale_fill_continuous(low = "blue", high = "red", na.value = "grey50", trans = "sqrt", guide= FALSE)
+                  
+                  # use this if you hate colors to get grey figure
+                  # p <- p + scale_fill_continuous(low = "grey50", high = "grey50", na.value = "grey50", trans = "sqrt", guide= FALSE)
+                  
+                  png("result.png", width = 768, height = 768, bg = "transparent")
+                  print(p)
+                  dev.off()
+                  pdf("result.pdf", width = 768, height = 768, bg = "transparent")
+                  print(p)
+                  dev.off()
+                  svg("result.svg", width = 768, height = 768, bg = "transparent")
+                  print(p)
+                  dev.off()
+
+                  # sort the data, then save .csv for the result
+                  dat.n <- dat.n[order(dat.n$ExecTime, decreasing = TRUE),]
+                  dat.n <- dat.n[,2:1]
+                  png("result.png", width = 1366, height = 768)
+                  print(p)
+                  dev.off()
+                  write.table(dat.n, "result.csv", sep = "\t", row.names = FALSE)
+
+                },
+                error=function(cond) {
+                  message("Package R caused an error!")
+                  message("Here's the original error message:")
+                  message(cond)
+                  # Choose a return value in case of error
+                  return(NA)
+                },
+                warning=function(cond) {
+                  message("Package R caused a warning:")
+                  message("Here's the original warning message:")
+                  message(cond)
+                  # Choose a return value in case of warning
+                  return(NULL)
+                },
+                finally={
+                  # NOTE:
+                  # Here goes everything that should be executed at the end,
+                  # regardless of success or error.
+
+                  # Do nothing
+                }
+                )
---
+++ bin/vim-plugins-profile.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+set -eu
+
+echo "Generating vim startup profile..."
+logfile="vim.log"
+
+if [ -f $logfile ]; then
+  # clear the log file first
+  rm $logfile
+fi
+
+vim --startuptime $logfile -c q
+
+
+echo 'Assuming your vimfiles folder as `~/.config/nvim/`'
+vimfilesDir="$HOME/.config/nvim/"
+
+plugDir=""
+if [ -d "${vimfilesDir}plugged" ]; then
+  echo "vim-plug has been detected."
+  plugDir="plugged"
+elif [ -d "${vimfilesDir}bundle" ]; then
+  echo "NeoBundle/Vundle/Pathogen has been detected."
+  plugDir="bundle"
+else
+  echo "Cannot tell your plugin-manager. Adjust this bash script\n"
+  echo "to meet your own needs for now."
+  echo 'Cue: `plugDir` variable would be a good starting place.'
+  exit 1
+fi
+
+
+
+echo "Parsing vim startup profile..."
+#logfile=hi.log
+grep $plugDir $logfile > tmp.log
+awk -F\: '{print $1}' tmp.log > tmp1.log
+# awk -F\: '{print $2}' tmp.log | awk -F\: '{print $2}' tmp.log | sed "s/.*${plugDir}\///g"|sed 's/\/.*//g' > tmp2.log
+awk -F\: '{print $2}' tmp.log | awk -F\: '{print $2}' tmp.log | perl -pe 's|.*?bundle/(.*?)/.*|\1|' | perl -pe 's|.*/(.*)|\1|' > tmp2.log
+paste -d ',' tmp1.log tmp2.log | tr -s ' ' ',' > profile.csv
+# rm tmp.log tmp1.log tmp2.log
+
+
+
+
+# Let's do the R magic!
+echo "Crunching data and generating profile plot ..."
+
+# Check if R is available
+echo " "
+type R > /dev/null 2>&1 || { echo -e >&2 "Package R is required but it's not installed. \nPlease install R using your package manager, \nor check out cran.r-project.org for instructions. \nAborting."; exit 1; }
+
+
+# Still here? Great! Let's move on!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+R --vanilla --quiet --slave --file="$DIR/vim-plugins-profile-plot.R"
+#R --vanilla --file="vim-plugins-profile-plot.R"  # or use this for debugging
+
+# we use result.csv, which is saved from R
+# delete profile.csv since it is used to feed into R
+rm profile.csv
+
+
+echo " "
+echo 'Your plugins startup profile graph is saved '
+echo 'as `result.png` under current directory.'
+echo " "
+echo "=========================================="
+echo "Top 10 Plugins That Slows Down Vim Startup"
+echo "=========================================="
+cat -n result.csv |head -n 10 # change this 10 to see more in this `Top List`
+echo "=========================================="
+
+echo "Done!"
+echo " "
