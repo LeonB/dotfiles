c4cefa4ca2efbaccb41783d8b55d7505f6805c73
Leon Bogaert <leon@tim-online.nl>
2016-05-08 22:50:58 +0200

    Move program_exists() to functions file

--- .bashrc.d/00_functions.sh
+++ .bashrc.d/00_functions.sh
@@ -14,3 +14,6 @@ function smart_prepend {
     echo ${new_path}${old_path:+:$old_path}
 }
 
+program_exists () {
+    type "$1" &> /dev/null;
+}
--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,20 +1,16 @@
 export GOPATH=$HOME/.go
 export PATH="$GOPATH/bin:$PATH"
 # export GOROOT="/usr/local/go"
-export GOROOT="$HOME/.linuxbrew/Cellar/go/1.5.1/libexec"
+# export GOROOT="$HOME/.linuxbrew/Cellar/go/1.6/libexec"
 
-# program_exists () {
-#     type "$1" &> /dev/null ;
-# }
-
-# if program_exists go; then
-#     function setupGOROOT()
-#     {
-#         local GOPATH=`which go`
-#         local GODIR=`dirname $GOPATH`
-#         local GOPATH_BREW_RELATIVE=`readlink $GOPATH`
-#         local GOPATH_BREW=`dirname $GOPATH_BREW_RELATIVE`
-#         export GOROOT=`cd $GODIR; cd $GOPATH_BREW/..; pwd`
-#     }
-#     setupGOROOT
-# fi
+if program_exists go; then
+    function setupGOROOT()
+    {
+        local GOPATH=`which go`
+        local GODIR=`dirname $GOPATH`
+        local GOPATH_BREW_RELATIVE=`readlink $GOPATH`
+        local GOPATH_BREW=`dirname $GOPATH_BREW_RELATIVE`
+        export GOROOT=`cd $GODIR; cd $GOPATH_BREW/../libexec; pwd`
+    }
+    setupGOROOT
+fi
