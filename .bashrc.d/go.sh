33b8caca0a6730a5a6fea3f0810825279f155316
Leon Bogaert <leon@tim-online.nl>
2016-05-12 16:36:17 +0200

    End game go init :)

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,7 +1,5 @@
 export GOPATH=$HOME/.go
 export PATH="$GOPATH/bin:$PATH"
-# export GOROOT="/usr/local/go"
-# export GOROOT="$HOME/.linuxbrew/Cellar/go/1.6/libexec"
 
 if program_exists go; then
     function setupGOROOT()
