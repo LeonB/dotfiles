fde42e6deb53e521e1e92b9ba907a6e2b7884482
Leon Bogaert <leon@tim-online.nl>
2016-08-14 16:54:31 +0200

    Installed go (1.7) manually

--- .bashrc.d/go.sh
+++ .bashrc.d/go.sh
@@ -1,14 +1,17 @@
 export GOPATH=$HOME/.go
 export PATH="$GOPATH/bin:$PATH"
 
+export GOROOT="$HOME/bin/go/"
+export PATH="$GOROOT/bin:$PATH"
+
 if program_exists go; then
     function setupGOROOT()
     {
         local GOPATH=`which go`
         local GODIR=`dirname $GOPATH`
-        local GOPATH_BREW_RELATIVE=`readlink $GOPATH`
+        local GOPATH_BREW_RELATIVE=`readlink -m $GOPATH`
         local GOPATH_BREW=`dirname $GOPATH_BREW_RELATIVE`
         export GOROOT=`cd $GODIR; cd $GOPATH_BREW/../libexec; pwd`
     }
-    setupGOROOT
+    # setupGOROOT
 fi
