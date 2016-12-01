653c78579ae86f9b1c4f8189f2a37ee7ad1b80d4
Leon Bogaert <leon@tim-online.nl>
2016-12-01 10:41:52 +0100

    Document setup @work

--- bin/set_resolution.sh
+++ bin/set_resolution.sh
@@ -2,3 +2,8 @@
 
 xrandr -q | grep "VGA1 connected" && xrandr --output LVDS1 --off --output VGA1 --auto
 xrandr -q | grep "HDMI1 connected" && xrandr --output LVDS1 --off --output HDMI1 --auto
+
+# enable monitor & vertical monitor
+xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --rotate left --right-of HDMI1
+# enable monitor & disable vertical monitor
+xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --off
