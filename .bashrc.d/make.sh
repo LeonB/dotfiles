e0cd1a055be8cbf532051377b1fce1be776d8d87
Leon Bogaert <leon@tim-online.nl>
2017-07-28 21:48:36 +0200

    Make MAKEFLAGS work on mac

--- .bashrc.d/make.sh
+++ .bashrc.d/make.sh
@@ -1,2 +1,8 @@
 # export allows variables to be passed to child processes
-export MAKEFLAGS="-j$(grep -c "^processor" /proc/cpuinfo)"
+if [ -f /proc/cpuinfo ]; then
+    export MAKEFLAGS="-j$(grep -c "^processor" /proc/cpuinfo)"
+fi
+
+if [ -x "$(command -v sysctl)" ]; then
+    export MAKEFLAGS="-j$(sysctl -n hw.ncpu)"
+fi
