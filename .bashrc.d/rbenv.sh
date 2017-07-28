77fdd27a4f3ea99ccc5eadd361cedd7ec187c34c
Leon Bogaert <leon@tim-online.nl>
2017-07-28 21:48:49 +0200

    Don't thow error if rbenv isn't installed

--- .bashrc.d/rbenv.sh
+++ .bashrc.d/rbenv.sh
@@ -1,3 +1,6 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init --no-rehash -)"
-(rbenv rehash &) 2> /dev/null
+
+if [ -x "$(command -v rbenv)" ]; then
+    eval "$(rbenv init --no-rehash -)"
+    (rbenv rehash &) 2> /dev/null
+fi
