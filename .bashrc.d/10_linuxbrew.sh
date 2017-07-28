a405ac299b60b42a1ac53db1fb25501125a20a17
Leon Bogaert <leon@tim-online.nl>
2017-07-28 21:45:15 +0200

    Also make homebrew work on mac

--- .bashrc.d/10_linuxbrew.sh
+++ .bashrc.d/10_linuxbrew.sh
@@ -1,16 +1,21 @@
-if [ -d $HOME/.linuxbrew/ ];then
-    LINUXBREW="$HOME/.dotfiles/.linuxbrew"
-    export HOMEBREW_LOGS="$LINUXBREW/Logs"
+if [ -d $HOME/.homebrew/ ]; then
+    BREW_HOME="$HOME/.dotfiles/.homebrew"
+elif [ -d $HOME/.linuxbrew/ ]; then
+    BREW_HOME="$HOME/.dotfiles/.linuxbrew"
+fi
 
-    export PATH="$LINUXBREW/bin:$PATH"
-    export PATH="$LINUXBREW/sbin:$PATH"
+if [ $BREW_HOME ]; then
+    export HOMEBREW_LOGS="$BREW_HOME/Logs"
+
+    export PATH="$BREW_HOME/bin:$PATH"
+    export PATH="$BREW_HOME/sbin:$PATH"
     # Break rbenv install / ruby-build?
-    export C_INCLUDE_PATH=`smart_prepend C_INCLUDE_PATH $LINUXBREW/include`
-    export C_INCLUDE_PATH=`smart_prepend C_INCLUDE_PATH $LINUXBREW/include/libxml2`
-    export LIBRARY_PATH=`smart_prepend LIBRARY_PATH $LINUXBREW/lib`
+    export C_INCLUDE_PATH=`smart_prepend C_INCLUDE_PATH $BREW_HOME/include`
+    export C_INCLUDE_PATH=`smart_prepend C_INCLUDE_PATH $BREW_HOME/include/libxml2`
+    export LIBRARY_PATH=`smart_prepend LIBRARY_PATH $BREW_HOME/lib`
 
-    export MANPATH="$LINUXBREW/share/man:$MANPATH"
-    export INFOPATH="$LINUXBREW/share/info:$INFOPATH"
+    export MANPATH="$BREW_HOME/share/man:$MANPATH"
+    export INFOPATH="$BREW_HOME/share/info:$INFOPATH"
 
-    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$LINUXBREW/opt/openssl"
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$BREW_HOME/opt/openssl"
 fi
