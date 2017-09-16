a00a05b1aa77d6997685e46239b7d21541682ebc
Leon Bogaert <leon@tim-online.nl>
2017-09-16 22:24:37 +0200

    Start using fzf / jump command

---
+++ .bashrc.d/fzf-marks.plugin.sh
@@ -0,0 +1,40 @@
+if [[ -z $BOOKMARKS_FILE ]] ; then
+    export BOOKMARKS_FILE="$HOME/.bookmarks"
+fi
+
+if [[ ! -f $BOOKMARKS_FILE ]]; then
+    touch $BOOKMARKS_FILE
+fi
+
+function mark() {
+    echo "$@ : $(pwd)" >> $BOOKMARKS_FILE
+}
+
+fzfcmd() {
+   [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+}
+
+function jump() {
+    local jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
+    if [[ -n ${jumpline} ]]; then
+        local jumpdir=$(echo "${jumpline}" | sed -n "s/.* \?: \(.*\)$/\1/p" | sed "s#~#$HOME#")
+        perl -p -i -e "s#${jumpline}\n##g" $BOOKMARKS_FILE
+        cd "${jumpdir}" && echo ${jumpline} >> $BOOKMARKS_FILE
+    fi
+}
+
+function dmark()  {
+    local marks_to_delete line
+    marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
+
+    if [[ -n ${marks_to_delete} ]]; then
+        while read -r line; do
+            perl -p -i -e "s#${line}\n##g" $BOOKMARKS_FILE
+        done <<< "$marks_to_delete"
+
+        echo "** The following marks were deleted **"
+        echo "${marks_to_delete}"
+    fi
+}
+
+bind '"\C-g":"jump\n"'
---
+++ .bashrc.d/fzf.sh
@@ -0,0 +1,1 @@
+export FZF_DEFAULT_OPTS='--height 40% --reverse'
