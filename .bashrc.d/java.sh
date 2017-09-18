dec0f424a9ee8c18a6852128a78a0b0c1aa7fa93
Leon Bogaert <leon@tim-online.nl>
2017-09-18 23:58:22 +0200

    Installed new java version

--- .bashrc.d/java.sh
+++ .bashrc.d/java.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=$HOME/bin/jre1.8.0_101/
+# export JAVA_HOME=$HOME/bin/jre1.8.0_112/
 export PATH=`smart_prepend PATH $JAVA_HOME/bin`
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on \
                       -Dswing.aatext=true \
