434c850901df8c3d50ea7f08af0045f17d804492
Leon Bogaert <leon@tim-online.nl>
2017-09-19 09:56:09 +0200

    Fixed bug

--- .bashrc.d/make.sh
+++ .bashrc.d/make.sh
@@ -1,8 +1,6 @@
 # export allows variables to be passed to child processes
 if [ -f /proc/cpuinfo ]; then
     export MAKEFLAGS="-j$(grep -c "^processor" /proc/cpuinfo)"
-fi
-
-if [ -x "$(command -v sysctl)" ]; then
+elif [ -x "$(command -v sysctl)" ]; then
     export MAKEFLAGS="-j$(sysctl -n hw.ncpu)"
 fi
