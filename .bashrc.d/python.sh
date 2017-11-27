c74c7a616e5d80ba1daaf5fb14c17d4be3e7218f
Leon Bogaert <leon@tim-online.nl>
2017-11-27 18:21:09 +0100

    Some python paths juggling

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,12 +1,11 @@
-export PYTHONPATH=""
-#export PYTHONPATH="${PYTHONPATH}:$HOME/.local/lib/python2.7/site-packages"
-#export PYTHONPATH="${PYTHONPATH}:$HOME/Workspaces/raw2hdr"
-#export PYTHONPATH="${PYTHONPATH}:$HOME/Workspaces/BCTimer/src/"
-#export PYTHONPATH="${PYTHONPATH}:/usr/lib/python2.7"
-#export PYTHONPATH="${PYTHONPATH}:$HOME/Workspaces/prive/otta"
+# export PYTHONPATH=""
+
+# use homebrew python
+# export PATH="$HOME/.dotfiles/.linuxbrew/Cellar/python/2.7.14/libexec/bin:$PATH"
+# export PATH="$HOME/.dotfiles/.linuxbrew/opt/python/libexec/bin:$PATH"
 
 # virtualenv and pip
 export VIRTUALENV_USE_DISTRIBUTE=1
 export PIP_RESPECT_VIRTUALENV=true
 #source $HOME/.local/bin/virtualenvwrapper.sh
-export PYTHONPATH="${PYTHONPATH}:$HOME/src/moppy-python"
+# export PYTHONPATH="${PYTHONPATH}:$HOME/src/moppy-python"
