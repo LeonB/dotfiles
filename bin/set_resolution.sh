1a0403f4472af1d1196c28afede174cf8215f38a
Leon Bogaert <leon@tim-online.nl>
2017-11-27 18:19:46 +0100

    Changed for new laptop

--- bin/set_resolution.sh
+++ bin/set_resolution.sh
@@ -4,6 +4,11 @@ xrandr -q | grep "VGA1 connected" && xrandr --output LVDS1 --off --output VGA1 -
 xrandr -q | grep "HDMI1 connected" && xrandr --output LVDS1 --off --output HDMI1 --auto
 
 # enable monitor & vertical monitor
-xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --rotate left --right-of HDMI1
+# xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --rotate left --right-of HDMI1
 # enable monitor & disable vertical monitor
-xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --off
+# xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --off
+
+# enable monitor & vertical monitor
+xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-2 --rotate left --right-of HDMI-1
+# enable monitor & disable vertical monitor
+xrandr --output eDP-1 --off --output HDMI-1 --auto --output DP-2 --off
