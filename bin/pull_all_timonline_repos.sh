36d533b5e9bb0072be177f552afe2836ce963877
Leon Bogaert <leon@tim-online.nl>
2017-11-27 18:19:24 +0100

    Better cloning of repositories

--- bin/pull_all_timonline_repos.sh
+++ bin/pull_all_timonline_repos.sh
@@ -24,17 +24,33 @@ repos=`echo "${repos}" | sort`
 
 for repo in $repos
 do
-    echo -n "Checking $repo"
+    echo "Checking $repo"
+    new=false
 
     if [ ! -d "${repo}" ]; then
-        echo " (cloning)"
+        new=true
+        echo -n " (cloning)"
         git clone "git@bitbucket.org:tim_online/${repo}.git" > /dev/null
-    else
-        echo " (updating)"
-        cd "${repo}"
+    fi
+
+    cd "${repo}"
+    echo -n "tracking all remote branches"
+    for branch in $(git branch --all | grep '^\s*remotes' | awk '{ print $1 }' | egrep --invert-match '(:?HEAD|master)$'); do
+        remote=`echo $branch | cut -f2 -d/`
+        branch=`echo $branch | cut -f3 -d/`
+        exists=`git show-ref refs/heads/$remote/$branch`
+        if [ ! -n "$exists" ]; then
+            git branch --track "$remote/$branch" "$remote/$branch"
+        fi
+    done
+
+    if [ "$new" = false ] ; then
+        echo -n " (updating)"
         git fetch --all > /dev/null
         git reset --hard FETCH_HEAD > /dev/null
         git clean -df > /dev/null
-        cd ..
     fi
+
+    cd ..
+    echo ""
 done
