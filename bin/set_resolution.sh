c8f56cb654b2b4c27b462b2d5de389da8d6100ae
Leon Bogaert <leon@tim-online.nl>
2019-06-09 00:01:13 +0200

    New laptop?

--- bin/set_resolution.sh
+++ bin/set_resolution.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-xrandr -q | grep "VGA1 connected" && xrandr --output LVDS1 --off --output VGA1 --auto
-xrandr -q | grep "HDMI1 connected" && xrandr --output LVDS1 --off --output HDMI1 --auto
+xrandr_output=$(xrandr -q)
+edp1_connected=$(echo "$xrandr_output" | grep "^eDP-1 connected")
+hdmi1_connected=$(echo "$xrandr_output" | grep "^HDMI-1 connected")
+dp1_connected=$(echo "$xrandr_output" | egrep "^DP-1 connected")
+dp2_connected=$(echo "$xrandr_output" | grep "^DP-2 connected")
 
-# enable monitor & vertical monitor
-# xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --auto --rotate left --right-of HDMI1
-# enable monitor & disable vertical monitor
-# xrandr --output LVDS1 --off --output HDMI1 --auto --output VGA1 --off
-
-# enable monitor & vertical monitor
-xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-2 --rotate left --right-of HDMI-1
-# enable monitor & disable vertical monitor
-xrandr --output eDP-1 --off --output HDMI-1 --auto --output DP-2 --off
+if [[ ! -z "$hdmi1_connected" && ! -z "$dp2_connected" ]]; then
+    # HDMI-1 and DP-2 connected
+    xrandr --output eDP-1 --off --output HDMI-1 --auto --primary --output DP-2 --rotate left --right-of HDMI-1
+    exit
+elif [[ ! -z "$dp1_connected" && ! -z "$dp2_connected" ]]; then
+    # HDMI-1 and DP-2 connected
+    xrandr --output eDP-2 --off --output DP-2 --auto --primary --output DP-1 --rotate left --right-of DP-2
+    exit
+elif [[ ! -z "$hdmi1_connected" ]]; then
+    # Only HDMI connected
+    xrandr --output eDP-1 --off --output HDMI1 --auto
+    exit
+elif [[ ! -z "$dp1_connected" ]]; then
+    # Only DP-1 connected
+    xrandr --output eDP-1 --off --output DP-1 --auto
+elif [[ ! -z "$dp2_connected" ]]; then
+    # Only DP-2 connected
+    xrandr --output eDP-1 --off --output DP-2 --auto
+    exit
+fi
